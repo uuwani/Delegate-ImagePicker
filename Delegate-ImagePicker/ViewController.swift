cd9baf30a3db03dc5de1cd4c32db956111069805
uuwani <paulchoi9502@gmail.com>
2019-07-06 13:33:26 +0900

    Initial Commit

---
+++ Delegate-ImagePicker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Delegate-ImagePicker
+//
+//  Created by Wanni on 06/07/2019.
+//  Copyright © 2019 Wanni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
